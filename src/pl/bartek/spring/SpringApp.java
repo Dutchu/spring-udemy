0fc8340e14d88ef6915da611ce0b32e07969ab97
Bartlomiej Blaszczyk <blaszczyk17@gmail.com>
2020-04-04 16:43:09 +0200

    first Spring framework XML config application

---
+++ src/pl/bartek/spring/SpringApp.java
@@ -0,0 +1,22 @@
+package pl.bartek.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringApp {
+    public static void main(String[] args) {
+        //load spring configuration file
+        ClassPathXmlApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //retrieve bean from spring container
+        Coach coach = applicationContext.getBean("coach", Coach.class);
+
+        //call methods on the bean
+        System.out.println(coach.training());
+
+        //close the context
+        applicationContext.close();
+
+    }
+
+}
